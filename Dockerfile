7210e2583f52922fb4a48838ff4cdc7d217cda77
nataliaguerrini1@gmail.com <nataliaguerrini1@gmail.com>
2024-10-12 17:22:07 +0200

    added logic to fetch messages from broker queue

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
 FROM node:20-alpine
 
 RUN mkdir -p home/app
-
-COPY . /home/app
 WORKDIR /home/app
+
+
+COPY package*.json ./
 RUN npm install
+
+COPY . .
+RUN npx prisma generate
 RUN npx tsc
 
 # Start RabbitMQ in the background and your Node.js app
-CMD npm install & npx tsc & node dist/app.js
+CMD npm install && npx tsc && node dist/app.js
 
 # CMD ["node", "dist/app.js"]
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,41 @@
 import * as amqp from "amqplib";
-import { log } from "console";
+import { PrismaClient } from "@prisma/client";
 
-log("broker started");
+console.log("broker started");
+
+const pullQueue = process.env.BROKER_QUEUE;
+const pushQueue = process.env.FETCHER_QUEUE;
+
+const firstJob = {
+  host: "https://en.wikipedia.org",
+  url: "https://en.wikipedia.org/wiki/Portal:Computer_programming",
+};
+
+const prisma = new PrismaClient();
 
 amqp.connect(process.env.MESSAGE_QUEUE).then((connection) => {
   connection.createChannel().then((channel) => {
-    channel.assertQueue(process.env.QUEUE);
-    channel.consume(process.env.QUEUE, (msg) => {
-      console.log(msg);
+    channel.assertQueue(pushQueue).then(() => {
+      channel.sendToQueue(pushQueue, Buffer.from(JSON.stringify(firstJob)));
+    });
+
+    channel.assertQueue(pullQueue).then(() => {
+      channel.consume(pullQueue, (msg) => {
+        const msgContent = JSON.parse(msg.content.toString());
+
+        prisma.site_info
+          .findFirst({
+            where: {
+              url: msgContent["url"],
+            },
+          })
+          .then((job) => {
+            if (!job) {
+              //se il job non è stato trovato allora inseriamo il messaggio in coda
+              channel.sendToQueue(pushQueue, Buffer.from(msg.content.toString()));
+            }
+          });
+      });
     });
   });
 });
